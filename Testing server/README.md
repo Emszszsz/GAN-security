3c64e2a1b8fdd5becc93128c49bb65aec9ebf49a
D23K4N <48097239+D23K4N@users.noreply.github.com>
2021-02-22 20:51:03 +0100

    Test version of test server

---
+++ Testing server/README.md
@@ -0,0 +1,2 @@
+Python3
+server starts on 127.0.0.1:8080
---
+++ Testing server/server_get.py
@@ -0,0 +1,36 @@
+# MDZ
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+import time
+
+hostName = "localhost"
+serverPort = 8000
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(open("test_page.html",'r').read()+open("comments.txt",'r').read()+open("test_page2.html",'r').read(), "utf-8"))
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        comment = post_data.decode('utf-8')
+        f = open("comments.txt", "a")
+        f.write(urllib.parse.unquote(comment[4:])+"\n")
+        f.close()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(open("test_page.html",'r').read()+open("comments.txt",'r').read()+open("test_page2.html",'r').read(), "utf-8"))
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")

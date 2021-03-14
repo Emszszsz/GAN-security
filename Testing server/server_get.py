91aafeb099a2a2a804b30fb885c010d9693cfa0c
D23K4N <48097239+D23K4N@users.noreply.github.com>
2021-03-14 14:26:10 +0100

    better web page
    check this out!

--- Testing server/server_get.py
+++ Testing server/server_get.py
@@ -15,15 +15,16 @@ class MyServer(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        comment = post_data.decode('utf-8')
+        comment = post_data.decode('utf-8').replace("+","%20")
         f = open("comments.txt", "a")
-        f.write(urllib.parse.unquote(comment[4:])+"\n")
+        f.write("<div id=\"space_box\"> &nbsp</div>\n"+"<div id=\"boxed\">"+urllib.parse.unquote(comment[4:])+"</div>\n")
         f.close()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes(open("test_page.html",'r').read()+open("comments.txt",'r').read()+open("test_page2.html",'r').read(), "utf-8"))
-if __name__ == "__main__":        
+if __name__ == "__main__": 
+    open('comments.txt', 'w').close()
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
